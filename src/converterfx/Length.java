1b97c1b9dce17c89f2e1b997dbbdefab0c1ffeb9
Pittayut Sothanakul <pittayut.sot@ku.th>
2018-03-09 17:54:28 +0700

    commit AU

--- src/converterfx/Length.java
+++ src/converterfx/Length.java
@@ -1,7 +1,7 @@
 package converterfx;
 
 public enum Length {
-	Meter(1.0), Kilometer(1000.0), Mile(1609.344), Foot(0.30480), Wa(2.00000),;
+	Meter(1.0), Kilometer(1000.0), Mile(1609.344), Foot(0.30480), Wa(2.00000), AU(149597870700.0);
 
 	private final double value;
 
